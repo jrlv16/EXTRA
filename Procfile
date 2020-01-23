1123b7e85f4332581aba038df81e614e01f3a34d
jrlv16 <jl062705@sfr.fr>
2020-01-23 08:48:00 +0100

    config pour Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn extra_project.wsgi --log-file -
--- extra_project/settings.py
+++ extra_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0pv(%#6=p9s4n7^w125@4t_mqa+wu8fd0wy##01#&s!@yoieg*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://extra-1.herokuapp.com/', ]
 
 
 # Application definition
@@ -79,16 +81,21 @@ WSGI_APPLICATION = 'extra_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'extraapp',
-        'USER': 'extraappadmin',
-        'PASSWORD': 'extraapppass',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'amazonaws.com' in os.environ
+    default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+    DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': 'extraapp',
+            'USER': 'extraappadmin',
+            'PASSWORD': 'extraapppass',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': {
@@ -135,9 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-PHONENUMBER_DEFAULT_REGION = 'FR'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+PHONENUMBER_DEFAULT_REGION = 'FR'
 PHONENUMBER_DB_FORMAT = 'E164'
 
 GEOTH_LIBRARY_PATH = 'C:\\OSGeo4W64\\bin'
